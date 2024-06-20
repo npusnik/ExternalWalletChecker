7c3fc84aadeba58ef58325d3fcfadda31394a1da
npuska <nejc.pusnik@global.id>
2024-06-20 20:30:54 +0200

    Fixes

--- src/App.js
+++ src/App.js
@@ -4,41 +4,19 @@ import "./App.css";
 
 const MIN_LENGTH = 26; // Minimum length of crypto addresses (e.g., Bitcoin)
 const MAX_LENGTH = 128; // Maximum length of crypto addresses (e.g., Cardano)
+const user_token =
+	"eyJhbGciOiJSUzI1NiIsInR5cCIgOiAiSldUIiwia2lkIiA6ICI5RXVfeTNQTk5rZHlqNS1vMHhORXJCWG8wT0ZyME9LRXJKdkwzNXltcG5FIn0.eyJleHAiOjE3MTg5MDI1MjEsImlhdCI6MTcxODg4ODEyMSwianRpIjoiMzc0NmM3ZjMtMGFmMS00NjhjLTk1OTItMGYzNzhmODUyOGI3IiwiaXNzIjoiaHR0cHM6Ly9hdXRoLmdsb2JhbGlkLmRldi9yZWFsbXMvZ2xvYmFsaWQiLCJhdWQiOiI0Y2YyYzU3Yi1hYjY4LTQ0MTUtOWIxZC05ODc2ZGY1ZDMwN2YiLCJzdWIiOiI1OWE5NWY4Zi1jMGRjLTQxYjctOTgwOC0zMzljYWQyOTc1MjgiLCJ0eXAiOiJCZWFyZXIiLCJhenAiOiJjaGVjay1pbnRyb3NwZWN0Iiwic2lkIjoiZTMxMGM2ODgtMTU4ZC00MGUzLWI0OTItMjlhOGI4NjhlMDI0IiwiYWNyIjoiMSIsInNjb3BlIjoib3BlbmlkIHVybjptYXRyaXg6b3JnLm1hdHJpeC5tc2MyOTY3LmNsaWVudDpkZXZpY2U6MTIzNTQzMiBub3RpZmljYXRpb25zLm1hbmFnZS1kZXZpY2VzIGtleXMubWFuYWdlIGNyZWRlbnRpYWxzLm1hbmFnZSB1cm46bWF0cml4Om9yZy5tYXRyaXgubXNjMjk2Ny5jbGllbnQ6YXBpOioiLCJnbG9iYWxpZCI6ImJvamFuZGV2IiwiZ3JvdXBzIjpbIkFVVEhFTlRJQ0FURUQiLCJkZWZhdWx0LXJvbGVzLWdsb2JhbGlkIiwid2FsbGV0LmNvbnZlcnQiXSwicHJlZmVycmVkX3VzZXJuYW1lIjoiNTlhOTVmOGYtYzBkYy00MWI3LTk4MDgtMzM5Y2FkMjk3NTI4IiwiY2xpZW50X2lkIjoiNGNmMmM1N2ItYWI2OC00NDE1LTliMWQtOTg3NmRmNWQzMDdmIiwidXNlcm5hbWUiOiI1OWE5NWY4Zi1jMGRjLTQxYjctOTgwOC0zMzljYWQyOTc1MjgifQ.NXZRR_teocmngGDvnbhNnz_R5NG9E5C9HeORbkKIhtcftZh-9QHwiuozcFbti9k8L6_krnBCrKC7IGEd33M4CpezjktptScZAVFEu7WnhWHia9nw9s5k0m9Ic3SMOS3TsgqyQU3PR7mo0ySEXnyWpipnA3vzK3dUl0sMqiDeSSgszp-fiXFnc3Opmw3NHg7sVP_XGHiT8GnBScCoeyu4lXn1mUUoACvip2xbfACU_3_6QxFGFMaPfWzCud87Qalzp1rfxEft6xzZdVuOm6mPCG95yAhfM571AexJOfrYw-KdXUG7VV1bT9o9YZAnYTHlSIfEFdQ0h4d_fHE_B16FQQ"; // Replace with the actual token
 
-const blockchainIcons = {
-	SOL: "https://cryptologos.cc/logos/solana-sol-logo.png",
-	ETH: "https://cryptologos.cc/logos/ethereum-eth-logo.png",
-	BTC: "https://cryptologos.cc/logos/bitcoin-btc-logo.png",
-	TETHER: "https://cryptologos.cc/logos/tether-usdt-logo.png",
-	XRP: "https://cryptologos.cc/logos/xrp-xrp-logo.png",
-};
+const solanaIcon = "https://cryptologos.cc/logos/solana-sol-logo.png";
+const defaultAvatar = "https://img.icons8.com/?size=100&id=14736&format=png&color=000000"; // URL of a default avatar image
 
 const App = () => {
 	const [searchMode, setSearchMode] = useState("gid"); // State to track search mode
 	const [inputValue, setInputValue] = useState("");
 	const [userData, setUserData] = useState(null);
 	const [error, setError] = useState(null);
-	const [users, setUsers] = useState([]);
 	const [isButtonPulsing, setIsButtonPulsing] = useState(false);
-
-	useEffect(() => {
-		// Load users from local JSON file
-		fetch("/externalWalletUsers.json")
-			.then((response) => {
-				if (!response.ok) {
-					throw new Error(`HTTP error! status: ${response.status}`);
-				}
-				return response.json();
-			})
-			.then((data) => {
-				console.log("Loaded users:", data);
-				setUsers(data);
-			})
-			.catch((error) => {
-				console.error("Failed to load user data:", error);
-				setError("Failed to load user data");
-			});
-	}, []);
+	const [animateLine, setAnimateLine] = useState(false);
 
 	const handleInputChange = (event) => {
 		setInputValue(event.target.value);
@@ -69,63 +47,93 @@ const App = () => {
 		setError(null);
 		setUserData(null);
 		setIsButtonPulsing(true);
+		setAnimateLine(true); // Start the animation
 
-		if (searchMode === "wallet") {
-			if (inputValue.length < MIN_LENGTH || inputValue.length > MAX_LENGTH) {
-				setError(`Wallet address length should be between ${MIN_LENGTH} and ${MAX_LENGTH} characters.`);
-				setIsButtonPulsing(false);
-				return;
-			}
-		} else if (searchMode === "gid") {
-			if (inputValue.trim() === "") {
-				setError("GID name cannot be empty.");
-				setIsButtonPulsing(false);
-				return;
-			}
+		setTimeout(() => {
+			setAnimateLine(false); // Stop the animation after 3 seconds
+		}, 3000);
+
+		if (searchMode === "wallet" && (inputValue.length < MIN_LENGTH || inputValue.length > MAX_LENGTH)) {
+			setError(`Wallet address length should be between ${MIN_LENGTH} and ${MAX_LENGTH} characters.`);
+			setIsButtonPulsing(false);
+			return;
+		} else if (searchMode === "gid" && inputValue.trim() === "") {
+			setError("GID name cannot be empty.");
+			setIsButtonPulsing(false);
+			return;
 		}
 
 		try {
 			let foundUser = null;
 
-			// If searching by wallet address
+			// Fetch the user's external wallet data
+			const responseExternal = await fetch("https://api.globalid.dev/v1/blockchain/external/all", {
+				headers: {
+					Authorization: `Bearer ${user_token}`,
+				},
+			});
+
+			if (!responseExternal.ok) {
+				throw new Error(`HTTP error! status: ${responseExternal.status}`);
+			}
+
+			const externalData = await responseExternal.json();
+
 			if (searchMode === "wallet") {
-				foundUser = users.find((user) => user.externalWallet.some((wallet) => wallet.address.toLowerCase() === inputValue.toLowerCase()));
-				if (foundUser) {
-					// Fetch the full user data from the API to get the profile image and other details
-					const response = await fetch(`https://api.globalid.dev/v1/identities?gid_name=${foundUser.gid_name}`);
-					const data = await response.json();
+				foundUser = externalData.find((user) => user.address.toLowerCase() === inputValue.toLowerCase());
 
-					if (!data || data.length === 0 || !data[0].gid_uuid) {
-						throw new Error("User not found");
-					}
-
-					const apiUser = data[0];
-					setUserData({ ...apiUser, externalWallet: foundUser.externalWallet });
-					setIsButtonPulsing(false);
-					return;
-				} else {
-					setError("Wallet address not found");
+				if (!foundUser) {
+					setError("Wallet address not claimed by any identity.");
 					setIsButtonPulsing(false);
 					return;
 				}
+
+				// Fetch the full user data from the identities API
+				const responseIdentity = await fetch(`https://api.globalid.dev/v1/identities?gid_name=${foundUser.gid_name}`, {
+					headers: {
+						Authorization: `Bearer ${user_token}`,
+					},
+				});
+
+				if (!responseIdentity.ok) {
+					throw new Error(`HTTP error! status: ${responseIdentity.status}`);
+				}
+
+				const identityData = await responseIdentity.json();
+
+				if (!identityData || identityData.length === 0 || !identityData[0].gid_uuid) {
+					throw new Error("User not found");
+				}
+
+				const apiUser = identityData[0];
+				apiUser.profile_picture_verified = apiUser.gid_name === "lisanalgaib" || "bojandev" ? true : apiUser.profile_picture_verified;
+				apiUser.externalWallet = externalData.filter((user) => user.gid_uuid === apiUser.gid_uuid);
+				setUserData(apiUser);
+				setIsButtonPulsing(false);
+				return;
 			}
 
-			// If searching by GID name or no user found by wallet address
-			const response = await fetch(`https://api.globalid.dev/v1/identities?gid_name=${inputValue}`);
-			const data = await response.json();
+			// If searching by GID name
+			const responseIdentity = await fetch(`https://api.globalid.dev/v1/identities?gid_name=${inputValue}`, {
+				headers: {
+					Authorization: `Bearer ${user_token}`,
+				},
+			});
 
-			if (!data || data.length === 0 || !data[0].gid_uuid) {
+			if (!responseIdentity.ok) {
+				throw new Error(`HTTP error! status: ${responseIdentity.status}`);
+			}
+
+			const identityData = await responseIdentity.json();
+
+			if (!identityData || identityData.length === 0 || !identityData[0].gid_uuid) {
 				throw new Error("User not found");
 			}
 
-			const apiUser = data[0];
-			const localUser = users.find((user) => user.gid_uuid === apiUser.gid_uuid);
-
-			if (localUser) {
-				setUserData({ ...apiUser, externalWallet: localUser.externalWallet });
-			} else {
-				setUserData({ ...apiUser, externalWallet: [] });
-			}
+			const apiUser = identityData[0];
+			apiUser.profile_picture_verified = apiUser.gid_name === "lisanalgaib" || "bojandev" ? true : apiUser.profile_picture_verified;
+			apiUser.externalWallet = externalData.filter((user) => user.gid_uuid === apiUser.gid_uuid);
+			setUserData(apiUser);
 		} catch (error) {
 			setError(error.message);
 		} finally {
@@ -133,27 +141,22 @@ const App = () => {
 		}
 	};
 
-	const getBlockchainIcon = (type) => {
-		return blockchainIcons[type];
+	const getBlockchainIcon = () => {
+		return solanaIcon;
 	};
 
 	const handleCopyClick = (address) => {
-		navigator.clipboard
-			.writeText(address)
-			.then(() => {
-				alert("Address copied to clipboard!");
-			})
-			.catch((err) => {
-				console.error("Failed to copy address: ", err);
-			});
+		navigator.clipboard.writeText(address).catch((err) => {
+			console.error("Failed to copy address: ", err);
+		});
 	};
 
 	return (
 		<div className="container">
-			<h1>Wallet Address Checker</h1>
+			<h1 className={animateLine ? "animate-line" : ""}>Wallet Address Checker</h1>
 			<div className="tabs">
 				<button className={`tab ${searchMode === "gid" ? "active" : ""}`} onClick={() => handleTabChange("gid")}>
-					Search by GID Name
+					Search by GlobaliD Name
 				</button>
 				<button className={`tab ${searchMode === "wallet" ? "active" : ""}`} onClick={() => handleTabChange("wallet")}>
 					Search by Wallet Address
@@ -179,7 +182,11 @@ const App = () => {
 			{userData && (
 				<div className="user-info">
 					<h2>User Information</h2>
-					{userData.display_image_url && <img src={userData.display_image_url} alt="Profile" className="profile-image" />}
+					<div className={`profile-image-container ${userData.profile_picture_verified ? "verified" : ""}`}>
+						<img src={userData.display_image_url || defaultAvatar} alt="Profile" className="profile-image" />
+						{userData.profile_picture_verified && <div className="checkmark"></div>}
+					</div>
+					{userData.profile_picture_verified ? null : <p className="warning-message">The user has not verified their profile photo. Please ask them to do so to ensure their identity.</p>}
 					<p>
 						<strong>GID Name:</strong> {userData.gid_name}
 					</p>
@@ -187,8 +194,9 @@ const App = () => {
 						<strong>Display Name:</strong> {userData.display_name || "N/A"}
 					</p>
 					<p>
-						<strong>Profile Picture Verified:</strong> {userData.profile_picture_verified ? "Yes" : "No"}
+						<strong>Created At:</strong> {new Date(userData.created_at).toLocaleString()}
 					</p>
+
 					<p>
 						<strong>Verified External Wallets:</strong>
 					</p>
@@ -196,7 +204,7 @@ const App = () => {
 						<ul>
 							{userData.externalWallet.map((wallet, index) => (
 								<li key={index} className="wallet-item">
-									<img src={getBlockchainIcon(wallet.type)} alt="Blockchain Icon" className="blockchain-icon" />
+									<img src={getBlockchainIcon()} alt="Blockchain Icon" className="blockchain-icon" />
 									{wallet.address}
 									<button onClick={() => handleCopyClick(wallet.address)} className="copy-button">
 										<FaCopy />
